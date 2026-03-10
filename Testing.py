a6c5ea482cb50ec770cd304c86faf097afcd947b
Itachi099634 <mansiratsingh2609@gmail.com>
2026-03-10 18:17:04 +0530

    first commit

--- Testing.py
+++ Testing.py
@@ -1,9 +1,9 @@
-import Pywhatkit
+import pywhatkit
 
 print("Simple WhatsApp Chatbot")
 
-name = input("Batko: ")
-number = input("919643292600: ")
+name = input("Enter your name: ")
+number = input("Enter the phone number (with country code): ")
 message = input("Enter the message you want to send: ")
 
 print("Sending message...")
@@ -11,3 +11,4 @@ print("Sending message...")
 pywhatkit.sendwhatmsg_instantly(number, message)
 
 print("Message sent successfully!")
+
